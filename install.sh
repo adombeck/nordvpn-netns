2e6566ee4caae1b4a778618901bac482a0268b65
Adrian Dombeck <adrian.dombeck@gmail.com>
2023-12-19 17:52:01 +0100

    Install /etc/netns/nordvpn/resolv.conf
    This file configures NordVPN DNS servers to avoid leaking DNS requests
    to the default DNS servers. It's automatically bind-mounted over
    /etc/resolv.conf when entering the network namespace via
    `ip netns exec nordvpn`.

--- install.sh
+++ install.sh
@@ -15,4 +15,7 @@ install -m 755 "${DIR}/nordvpn-netns" /usr/local/sbin
 install -d -m 755 /usr/local/lib/systemd/system
 install -m 644 "${DIR}/systemd/nordvpn-netns.service" /usr/local/lib/systemd/system
 
+install -d -m 700 /etc/netns/nordvpn
+install -m 600 "${DIR}/resolv.conf" /etc/netns/nordvpn
+
 systemctl daemon-reload
