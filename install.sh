d2446359b694886708d17d5137d05619dae173a4
Adrian Dombeck <adrian.dombeck@gmail.com>
2023-12-19 14:24:09 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 install -d -m 755 /usr/local/sbin
-install -m 755 wg_namespace_cli /usr/local/sbin
+install -m 755 "${DIR}/wg_namespace_cli" /usr/local/sbin
 
 install -d -m 755 /usr/local/lib/systemd/system
-install -m 644 systemd/wg-netnamespace@.service /usr/local/lib/systemd/system
+install -m 644 "${DIR}/systemd/wg-netnamespace@.service" /usr/local/lib/systemd/system
 
 systemctl daemon-reload
