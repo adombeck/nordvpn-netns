19f10e4b19f80dc5eb2ca0ffe619cd40f4070c5e
Adrian Dombeck <adrian.dombeck@gmail.com>
2023-12-19 17:23:47 +0100

    Rename to nordvpn-netns
    The code is NordVPN specific now, so we can also reflect that in the
    name.

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 install -d -m 755 /usr/local/sbin
-install -m 755 "${DIR}/wg_namespace_cli" /usr/local/sbin
+install -m 755 "${DIR}/nordvpn-netns" /usr/local/sbin
 
 install -d -m 755 /usr/local/lib/systemd/system
-install -m 644 "${DIR}/systemd/wg-netnamespace.service" /usr/local/lib/systemd/system
+install -m 644 "${DIR}/systemd/nordvpn-netns.service" /usr/local/lib/systemd/system
 
 systemctl daemon-reload
