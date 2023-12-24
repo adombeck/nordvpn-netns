ce4e65699cfde0fb2b3ebce52c4dc60d83fac5ff
Adrian Dombeck <adrian.dombeck@gmail.com>
2023-12-24 17:23:57 +0100

    Fix resolv.conf permissions

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ install -m 755 "${DIR}/nordvpn-netns" /usr/local/sbin
 install -d -m 755 /usr/local/lib/systemd/system
 install -m 644 "${DIR}/systemd/nordvpn-netns.service" /usr/local/lib/systemd/system
 
-install -d -m 700 /etc/netns/nordvpn
-install -m 600 "${DIR}/resolv.conf" /etc/netns/nordvpn
+install -d -m 755 /etc/netns/nordvpn
+install -m 644 "${DIR}/resolv.conf" /etc/netns/nordvpn
 
 install -d -m 700 /etc/nordvpn-netns
 
